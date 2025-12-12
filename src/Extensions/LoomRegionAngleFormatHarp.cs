a5cf450ab7f31260f50115d3c755c9fd81d92711
aspaNeuro <a.almeida@neurogears.org>
2025-12-12 16:38:34 +0000

    Add LoomRegionState and LoomRegion Angle to logs

---
+++ src/Extensions/LoomRegionAngleFormatHarp.cs
@@ -0,0 +1,35 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai.Harp;
+
+[Combinator]
+[Description("Transforms a Loom Zone Angle into a harp message for logging")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class LoomRegionAngleFormatHarp
+{
+public int Address { get; set; }
+    public IObservable<HarpMessage> Process(IObservable<Timestamped<Tuple<int, int, double>>> source)
+    {
+        return source.Select(x =>
+            {
+                var loomZoneAngle = x.Value;
+                var timestamp = x.Seconds;
+                return HarpMessage.FromSingle(
+                    Address,
+                    timestamp,
+                    MessageType.Event,
+                    
+                    //Blob Id
+                    (float)loomZoneAngle.Item1,
+                    //Zone Id
+                    (float)loomZoneAngle.Item2,
+                    //ZoneState
+                    (float)loomZoneAngle.Item3);
+                    
+            });
+    }
+}
---
+++ src/Extensions/LoomRegionStateFormatHarp.cs
@@ -0,0 +1,35 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using Bonsai.Harp;
+
+[Combinator]
+[Description("Transforms a Loom Zone State into a harp message for logging")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class LoomRegionStateFormatHarp
+{
+public int Address { get; set; }
+    public IObservable<HarpMessage> Process(IObservable<Timestamped<Tuple<int, int, bool>>> source)
+    {
+        return source.Select(x =>
+            {
+                var loomZoneState = x.Value;
+                var timestamp = x.Seconds;
+                return HarpMessage.FromByte(
+                    Address,
+                    timestamp,
+                    MessageType.Event,
+                    
+                    //Blob Id
+                    (byte)loomZoneState.Item1,
+                    //Zone Id
+                    (byte)loomZoneState.Item2,
+                    //ZoneState
+                    (byte)(loomZoneState.Item3?1:0));
+                    
+            });
+    }
+}
