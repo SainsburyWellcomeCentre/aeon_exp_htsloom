06972183635dbd8beb13376978c26c3972c9486e
aspaNeuro <a.almeida@neurogears.org>
2026-02-12 09:02:01 +0000

    Update Deploy.ps1 documentation
    Mentioned in #23

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,10 +1,9 @@
 <#
 This script performs the following actions:
 1. Cleans the .bonsai directory by removing untracked files and directories.
-2. Install bonsai by running setup.ps1 in bonsai folder.
-7. Clean the python .venv by removing untracked files and directories.
-8. Install uv if not installed.
-9. Install python environment .venv with uv using the pyproject.toml specs
+2. Clean the python .venv by removing untracked files and directories.
+3. Install uv if not installed.
+4. Install python environment .venv with uv using the pyproject.toml specs
 #>
 
 git clean -fdx .bonsai
