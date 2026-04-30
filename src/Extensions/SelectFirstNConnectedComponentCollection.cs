47f78270c6694550c645d764fdc7f2d6085ca438
aspaNeuro <a.almeida@neurogears.org>
2026-04-30 13:50:34 +0100

    Fix issue with reflection Reflection is all ways comming from a blob at the bottom of the image. When there is more than one blob we select the one that is closer to the top of image.

--- src/Extensions/SelectFirstNConnectedComponentCollection.cs
+++ src/Extensions/SelectFirstNConnectedComponentCollection.cs
@@ -5,23 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using Bonsai.Vision;
+using OpenCV.Net;
+using System.Diagnostics.Eventing.Reader;
 
 [Combinator]
 [Description("")]
 [WorkflowElementCategory(ElementCategory.Transform)]
 public class SelectFirstNConnectedComponentCollection
 {
-    public int NumberOfElements { get; set; }
+    public double minArea { get; set; }
+
     public IObservable<ConnectedComponentCollection> Process(IObservable<ConnectedComponentCollection> source)
     {
         return source.Select(value =>
         {
+            if(value.Count <= 0)
+            {
+                return value;
+            }
             var imageSize = value.ImageSize;
             ConnectedComponentCollection result = new ConnectedComponentCollection(imageSize);
-            var max = (value.Count > NumberOfElements)?NumberOfElements: value.Count; 
-            for(int count=0; count < max; count++)
-                result.Add(value[count]);
-            return result   ;
+
+            if (value.Count > 1)
+            {
+                if (value[0].Area > minArea && value[1].Area > 200)
+                    result.Add((value[0].Centroid.Y < value[1].Centroid.Y) ? value[0] : value[1]);
+                else
+                    result.Add(value[0]);
+            }
+            else
+                result.Add(value[0]);
+            return result;
         });
     }
 }
